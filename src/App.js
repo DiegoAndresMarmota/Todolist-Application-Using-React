6abfa01f160f26d869d38d8582380681fc337b80
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-09-23 19:18:50 -0300

    Actualización

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import ListaDeTareas from "./componentes/ListaDeTareas";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="aplicacion-tareas">
+      <div className="contenedor"></div>
+      <div className="tareas-lista-principal">
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
 }
 
 export default App;
